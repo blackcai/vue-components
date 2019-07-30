697c2d3c75ce9c87e33b67d693756ccb1c1ff103
blackcai <896339934@qq.com>
2019-07-30 21:50:08 +0800

    添加获取html功能

--- pages/table/index.vue
+++ pages/table/index.vue
@@ -22,8 +22,8 @@
       <el-card size="mini">
         <div class="text item">
           <el-button size="mini" @click="addTbody">添加一行表格</el-button>
-          <el-button size="mini" @click="hideBorder = true" v-if="!hideBorder">隐藏输入框边框</el-button>
-          <el-button size="mini" @click="hideBorder = false" v-if="hideBorder">显示输入框边框</el-button>
+          <el-button size="mini" @click="hideBorder = !hideBorder">{{ hideBorder ? '显示输入框边框' : '隐藏输入框边框' }}</el-button>
+          <el-button size="mini" @click="inputText = !inputText">{{ inputText ? '文字转输入框' : '输入框转换文字' }}</el-button>
           <el-input v-model="lineNumber" size="mini" placeholder="请输入行数" type="number" style="width:200px;" @keyup.enter.native="inputDelete">
             <el-button slot="append" @click="inputDelete">删除指定行数据</el-button>
           </el-input>
@@ -51,18 +51,27 @@
         </div>
       </el-card>
     </el-card>
-    <table style="width:1000px;border:1px solid #ccc;margin: 100px auto 100px auto;">
+    <table style="width:1000px;margin: 100px auto 100px auto;border-collapse: collapse;border-spacing: 0;" :style="{ border: `1px solid ${ color.border }` }" ref="table">
       <thead :style="{ background: color.headBg }">
       <tr>
-        <th v-for="(item, index) in thead" :key="index" style="text-align: left;padding:10px;" :style="{ borderRight: color.border, color: color.headColor }">{{ item }}</th>
+        <th v-for="(item, index) in thead" :key="index" style="text-align: left;padding:10px;" :style="{ borderRight: `1px solid ${color.border}`, color: color.headColor }">{{ item }}</th>
       </tr>
       </thead>
       <tbody>
-      <tr v-for="(item, index) in tbody" :key="index" style="border-top:1px solid #ccc;">
-        <td v-for="(c, j) in item" :key="j" style="padding:10px;" :style="{ borderRight: color.border, color: color.headColor }"><input type="text" v-model="tbody[index][j]" placeholder="" :style="{ border: hideBorder ? 'none' : '' }"></td>
+      <tr v-for="(item, index) in tbody" :key="index" :style="{ borderTop: `1px solid ${ color.border }` }">
+        <td v-for="(c, j) in item" :key="j" style="padding:10px;" :style="{ borderRight: `1px solid ${ color.border }`, color: color.headColor }">
+          <input type="text" v-if="!inputText" v-model="tbody[index][j]" placeholder="" :style="{ border: hideBorder ? 'none' : '' }">
+          <span v-if="inputText">{{ tbody[index][j] }}</span>
+        </td>
       </tr>
       </tbody>
     </table>
+    <el-card>
+      <div slot="header" class="clearfix">
+        <span>表格代码</span><el-button @click="getHtml" size="mini">获取表格代码</el-button>
+      </div>
+      <el-card>{{ html }}</el-card>
+    </el-card>
   </section>
 </template>
 
@@ -75,18 +84,21 @@
         theadValue: '',
         tbody: [],
         hideBorder: false,
+        inputText: false,
         lineNumber: 0,
         color: {
           headBg: '#99bbdd',
           border: '#ccc',
           headColor: '#545454',
           bodyColor: '#545454'
-        }
+        },
+        html: ''
       }
     },
     layout: 'bar',
     methods: {
       addThead() {
+        if (!this.theadValue.trim()) return
         this.thead.push(this.theadValue)
         this.theadValue = ''
         if (this.tbody.length) {
@@ -117,14 +129,35 @@
        this.tbody.push(arr)
       },
       inputDelete() {
+        if (!this.lineNumber) return
         // 最少保留一行数据
         // 实际比输入的少一行
-        const trueNumber = this.lineNumber - 1
-        if (this.lineNumber > this.tbody.length) {
-          this.$message('请输入正确的行数。')
-        } else {
-          this.tbody.splice(trueNumber, 1)
+        this.$confirm(`请确认您将删除第${this.lineNumber}行数据，此操作不可恢复。`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          const trueNumber = this.lineNumber - 1
+          if (this.lineNumber > this.tbody.length) {
+            this.$message('请输入正确的行数。')
+          } else {
+            this.tbody.splice(trueNumber, 1)
+          }
+        }).catch(() => {
+        })
+      },
+      getHtml() {
+        let isChange = false
+        if (!this.inputText) {
+          this.inputText = true
+          isChange = true
         }
+        setTimeout(() => {
+          this.html = this.$refs.table.outerHTML
+          if (isChange) {
+            this.inputText = false
+          }
+        }, 0)
       }
     }
   }
